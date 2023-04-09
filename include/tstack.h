6005ea8a4abe202ddd5ab3680371eb9f63f868e7
LizaShelkovskaya0705 <116936362+LizaShelkovskaya0705@users.noreply.github.com>
2023-04-09 09:39:33 +0300

    Update tstack.h

--- include/tstack.h
+++ include/tstack.h
@@ -1,22 +1,24 @@
 // Copyright 2021 NNTU-CS
 #ifndef INCLUDE_TSTACK_H_
 #define INCLUDE_TSTACK_H_
+#include <string>
 
 template<typename T, int size>
 class TStack{
  private:
     T arr[100];
-    int max;
+    int top;
+
  public:
-    TStack() :max(-1) {
-    void push(T value){
+    TStack() :max(-1) {}
+    void push(T value) {
       if (isFull()) {
         throw std::string("Stack - full");
       } else {
-        arr[++max ] = value;
+        arr[++max] = value;
       }
     }
-      const T& pop() {
+    const T& pop() {
       if (isEmpty()) {
         throw std::string("Stack - empty");
       } else {
@@ -33,4 +35,5 @@ class TStack{
       return arr[max];
     }
 };
+
 #endif  // INCLUDE_TSTACK_H_
